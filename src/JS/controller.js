2f3838f64ce725deefe1709f645c63a8c531295b
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-01-24 13:13:07 +0200

    Controller: add Pieces to Board♟

--- src/JS/controller.js
+++ src/JS/controller.js
@@ -5,14 +5,60 @@ import { Rook } from "./pieces/Rook";
 import { Queen } from "./pieces/Queen";
 import { Bishop } from "./pieces/Bishop";
 
-const testAllowed = function (test) {
-    console.log(`${test.name} Allowed Moves:${test.getAllowedMoves()}`);
+// const testAllowed = function (test) {
+//     console.log(`${test.name} Allowed Moves:${test.getAllowedMoves()}`);
+// }
+
+
+// testAllowed(new Knight(55, "white", "knight"));
+// testAllowed(new Pawn(55, "black", "pawn"));
+// testAllowed(new Rook(55, "white", "rook"));
+// testAllowed(new Queen(55, "white", "queen"));
+// testAllowed(new Bishop(55, "white", "bishop"));
+
+const getPieceSquare = (pos => document.querySelector(`[data-pos="${pos}"]`));
+const resetBoard = function (pieces) {
+
+    pieces.forEach(piece => {
+        const square = getPieceSquare(piece.getPos());
+        square.innerHTML = '';
+        const img = document.createElement("img");
+        let path = `${piece.color}-${piece.name}.png`
+        img.src = new URL(path, import.meta.url);
+        square.appendChild(img);
+        piece.addHandler(displayAvailable);
+    });
+
 }
 
+const addPawnsHelper = (starterPosition, color) => {
+    const pawns = [];
+    for (let i = starterPosition; i < starterPosition + 8; i++)
+        pawns.push(new Pawn(i, color, "pawn"));
+    return pawns;
+}
+const pieces = [
 
-testAllowed(new Knight(55, "white", "knight"));
-testAllowed(new Pawn(55, "black", "pawn"));
-testAllowed(new Rook(55, "white", "rook"));
-testAllowed(new Queen(55, "white", "queen"));
-testAllowed(new Bishop(55, "white", "bishop"));
+    new Rook(81, "white", "rook"),
+    new Knight(82, "white", "knight"),
+    new Bishop(83, "white", "bishop"),
+    new Queen(84, "white", "queen"),
+    new Knight(85, "white", "king"),
+    new Bishop(86, "white", "bishop"),
+    new Knight(87, "white", "knight"),
+    new Rook(88, "white", "rook"),
+    ...addPawnsHelper(71, "white"),
 
+    new Rook(11, "black", "rook"),
+    new Knight(12, "black", "knight"),
+    new Bishop(13, "black", "bishop"),
+    new Queen(14, "black", "queen"),
+    new Knight(15, "black", "king"),
+    new Bishop(16, "black", "bishop"),
+    new Knight(17, "black", "knight"),
+    new Rook(18, "black", "rook"),
+    ...addPawnsHelper(21, "black"),
+
+
+]
+resetBoard(pieces);

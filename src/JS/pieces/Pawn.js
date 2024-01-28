7cbd3526384c28a2a005b15e4c020f0b42b3f697
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-01-28 16:47:21 +0200

    Refactor: Update Pawn

--- src/JS/pieces/Pawn.js
+++ src/JS/pieces/Pawn.js
@@ -3,13 +3,32 @@ import { Piece } from "./Piece";
 export class Pawn extends Piece {
     constructor(pos, color, name) {
         super(pos, 'Pawn', color, name);
-    }
+        this.firstTwoStep = true;
 
+    }
+    setPos(position) {
+        this.pos = position;
+        this.firstTwoStep = false;
+    }
+    /*  Pawn Logic 
+     - Can move two squares on first move
+
+     */
+    validConrnerMove(destination) {
+        const destElement = document.querySelector(`[data-pos="${destination}"] img`);
+        if (!destElement) return false;
+        console.log(destElement);
+        return true;
+
+    }
     getAllowedMoves() {
         const sign = this.color === "white" ? -1 : 1;
-        return [this.pos + 10 * sign, this.pos + 20 * sign,
-        this.pos + 9 * sign, this.pos + 11 * sign // second two is for Attack moves
-        ].filter(poos => this.validPos(poos));
+
+        const cornerMoves = [this.pos + 9 * sign, this.pos + 11 * sign].filter(poos => this.validPos(poos) && this.validConrnerMove(poos));
+
+        const allowedMoves = [this.pos + 10 * sign, ...cornerMoves];
+        if (this.firstTwoStep) allowedMoves.push(this.pos + 20 * sign);
+        return allowedMoves.filter(poos => this.validPos(poos));
 
     }
 }
